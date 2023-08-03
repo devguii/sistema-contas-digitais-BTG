dbe40067db7ed050d1c8f0a8623bd9d2130fa4af
gui <guigon@alunos.utfpr.edu.br>
2023-08-03 17:53:50 -0300

    feat: add CreateCliente and CreateConta

--- src/modules/cliente/cliente.controller.ts
+++ src/modules/cliente/cliente.controller.ts
@@ -1,7 +1,18 @@
 import { Controller, Get, Post, Body, Put } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
+import { CreateClienteDto } from './dto/create-cliente.dto';
+import { CreateContaDto } from './dto/create-conta.dto';
 
 @Controller('cliente')
 export class ClienteController {
   constructor(private readonly clienteService: ClienteService) {}
+
+  @Get('clientes')
+  async getClientes() {
+    return this.clienteService.getClientes();
+  }
+  @Post('create')
+  async createCliente(@Body() createClienteDto: CreateClienteDto) {
+    return this.clienteService.createCliente(createClienteDto);
+  }
 }
--- src/modules/cliente/cliente.service.ts
+++ src/modules/cliente/cliente.service.ts
@@ -1,7 +1,52 @@
+import { CreateContaDto } from './dto/create-conta.dto';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateClienteDto } from './dto/create-cliente.dto';
+import { Cliente } from '@prisma/client';
 
 @Injectable()
 export class ClienteService {
   constructor(private readonly prisma: PrismaService) {}
+  async getClientes() {
+    return await this.prisma.cliente.findMany({
+      select: {
+        id: true,
+        nome: true,
+        data_nascimento: true,
+        endereco: true,
+        email: true,
+        senha: false,
+      },
+    });
+  }
+  async createCliente(createClienteDto: CreateClienteDto) {
+    return await this.prisma.cliente
+      .create({
+        data: {
+          nome: createClienteDto.nome,
+          data_nascimento: createClienteDto.data_nascimento,
+          endereco: createClienteDto.endereco,
+          email: createClienteDto.email,
+          senha: createClienteDto.senha,
+        },
+      })
+      .then(async () => {
+        const clienteData = await this.prisma.cliente.findUnique({
+          where: {
+            email: createClienteDto.email,
+          },
+        });
+
+        await this.prisma.conta.create({
+          data: {
+            cliente: {
+              connect: {
+                id: clienteData.id,
+              },
+            },
+            saldo: 0,
+          },
+        });
+      });
+  }
 }
