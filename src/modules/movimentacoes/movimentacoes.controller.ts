72a43c22339b04b1c8aac821ae5a42c61ccea765
gui <guigon@alunos.utfpr.edu.br>
2023-08-04 01:49:01 -0300

    feat: add Movimentacoes module

---
+++ src/modules/movimentacoes/movimentacoes.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { MovimentacoesService } from './movimentacoes.service';
+
+@Controller('movimentacoes')
+export class MovimentacoesController {
+  constructor(private readonly movimentacoesService: MovimentacoesService) {}
+
+  @Get(':numero_conta')
+  async consultaExtrato(@Param('numero_conta') numero_conta: number) {
+    return this.movimentacoesService.consultaExtrato(numero_conta);
+  }
+}
