85b18c0a782d6a0acb8a59db644943aa4a85e985
gui <guigon@alunos.utfpr.edu.br>
2023-08-04 01:55:22 -0300

    feat: import Conta, Movimentacoes in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ClienteModule } from './modules/cliente/cliente.module';
+import { ContaModule } from './modules/conta/conta.module';
+import { MovimentacoesModule } from './modules/movimentacoes/movimentacoes.module';
 
 @Module({
-  imports: [PrismaModule, ClienteModule],
+  imports: [PrismaModule, ClienteModule, ContaModule, MovimentacoesModule],
   controllers: [AppController],
   providers: [AppService],
 })
